edf6069b1aad3418096b0343ef0e9d3480c66313
lisiastek <patryklata11@gmail.com>
2024-07-25 16:47:02 +0200

    bugfix z nierozumieniem sie + komentarze

--- bin/lighting.py
+++ bin/lighting.py
@@ -27,32 +27,43 @@ class lightingManager(Loggable, Executor):
         # chunk: Chunk
         # block: Block
         
-        # basic values
+        # basic values (light from the sky) (1st iteration of light engine)
         for chunk in chunks.values():
             for x in range(0, int(chunk.SIZE.x)):
+                # set current light to sky light
                 currentLight = skyLight
                 
                 for y in range(0, int(chunk.SIZE.y)):
                     block = chunk.getBlockByTuple((x,y))
                     
+                    # if theres no block, continue
                     if block == None:
                         continue
                         # if currentLight == 15: print('s')
                         
-                    currentLight = max(block.lightValue - block.lightingAbsorption, 0)  
+                    # calculate strength of current light beam
+                    block.lightValue = currentLight
+                    currentLight = max(currentLight - block.lightingAbsorption, 0)  
+                    
+        # add additional light sources like torches of lava (2nd iteration of light engine) (not done yet)
                           
-        # check for neighbouring blocks
+        # check for neighbouring blocks add mix them together (3nd iteration of light engine) (this need some reworking (im considering creating additional table, not important that much, because that are only small bugs)
+        
+        # sort blocks
         chunkInCorrectOrder = dict(sorted(chunks.items(), key=lambda item: item[0]))
         # print(chunkInCorrectOrder)
         for chunk in chunkInCorrectOrder.values():
             for y in range(0, int(chunk.SIZE.y)):
                 for x in range(0, int(chunk.SIZE.x)):
                     
+                    # get block
                     block = chunk.getBlockByTuple((x,y))
                     
+                    # if no block continue
                     if block == None:
                         continue
                         
+                    # get block neighbours (if no neighbours then set 0, in the future it should get background tile instead)
                     up = block.getBlockUp()
                     up = 0 if up == None else up.lightValue
  
@@ -65,10 +76,12 @@ class lightingManager(Loggable, Executor):
                     left = block.getBlockLeft()
                     left = 0 if left == None else left.lightValue           
                         
+                    # calculate max of this values -2
                     block.lightValue = max(
                         up-2, down-2, right-1, left-1, 2, block.lightValue
                     ) - 2
-                        
+                       
+                    # tell block to recompile its image to fix the new assigned light value 
                     block.recompileLight()
                         
         
