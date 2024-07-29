cc9674e5163c5290bf2daff3fa2ddd4d2940aec0
lisiastek <patryklata11@gmail.com>
2024-07-29 02:20:29 +0200

    additional information

--- bin/tiles/_example.py
+++ bin/tiles/_example.py
@@ -1,9 +1,32 @@
+# external imports
 from pygame import Surface, Vector2
 from typing import Optional
+
+# internal imports
 from bin.map import Block
 from bin.map import Chunk
 from bin.abstractClasses import Executor, Reason
 from bin.event import Event
+
+
+# ----------------------
+# ADDITIONAL INFOMATION
+# ----------------------
+# 1. You can access any texture that you've specified in class variables by typing just self.[TEXTURE_NAME]_RENDER, 
+# because during intialization of the namespace, resourceManager leaves this variable.
+# you shall not use pygame.image.load() or similar things because that slower 
+# (namespace is intialized once and you use the same texture for multiple objects)
+# 2. all files starting with _ will be ignored and won't be loaded by resourceManager (they won't be loaded into game)
+# 3. you shouldn't include methods in your class that you want to change! 
+# it will just unnecessarily make loading time and size of the file longer/bigger
+# 4. File name, class name and string id should be as following: [stringID].py, [stringID], [stringID]
+# 5. you can include as many things in this file as you want (even imports or another classes),
+# you only can't include any others blocks or entities.
+# If you want them use existing methods or resourceManager
+# 6. idInt works only for blocks and string id is prioritized
+# 7. idInts and stringIds can't be ambigious, you can't use them over again. Every id have to specify one block.
+# 8. paths for images, etc. starts with "resources/" you don't need to add that.
+# 9. hi?
 
 
 class BLOCKID(Block):
